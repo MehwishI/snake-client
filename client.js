6360437f3e22ed647aa41844983146c29275faad
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-12 22:39:16 -0500

    adding modularity by separating connect function from play.js to client.js

---
+++ client.js
@@ -0,0 +1,14 @@
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  conn.setEncoding("utf-8");
+  conn.on("data", (data) => {
+    console.log(`Server says: ${data}`);
+  });
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,18 +1,9 @@
 const net = require("net");
 
+const connect = require("./client");
+
 //establish connection
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  conn.setEncoding("utf-8");
-  conn.on("data", (data) => {
-    console.log(`Server says: ${data}`);
-  });
-  return conn;
-};
 console.log("Connecting ...");
+
 connect();
