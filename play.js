84e276cdb1a27ae9be5febfc0c56358c207d0249
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-13 20:24:28 -0500

    setupInput, handleUserInput(), stdin, Ctrl C

--- play.js
+++ play.js
@@ -1,9 +1,31 @@
 const net = require("net");
 
 const connect = require("./client");
+const { stdin } = require("process");
 
 //establish connection
 
 console.log("Connecting ...");
 
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function () {
+  stdin.on("data", (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+
+  setupInput();
+};
