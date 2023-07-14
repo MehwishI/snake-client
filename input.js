00e2e9e9a0ca89216b792d696785d0c88359f577
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-13 21:07:58 -0500

    sending commands to move to server

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 const { stdin } = require("process");
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf-8");
@@ -15,6 +17,22 @@ const handleUserInput = function () {
   stdin.on("data", (key) => {
     if (key === "\u0003") {
       process.exit();
+    }
+    if (key === "w") {
+      //sending command to the server to move up
+      connection.write("Move: up");
+    }
+    if (key === "a") {
+      //sending command to the server to move left
+      connection.write("Move: left");
+    }
+    if (key === "s") {
+      //sending command to the server to move down
+      connection.write("Move: down");
+    }
+    if (key === "d") {
+      //sending command to the server to move right
+      connection.write("Move: right");
     }
   });
 };
