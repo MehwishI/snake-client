7e380125f5c5745c54087ab66d1c85891622b92c
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-13 19:40:37 -0500

    name command

--- client.js
+++ client.js
@@ -1,13 +1,20 @@
+const net = require("net");
+
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541,
+  });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SNK");
   });
 
   conn.setEncoding("utf-8");
   conn.on("data", (data) => {
     console.log(`Server says: ${data}`);
   });
+
   return conn;
 };
 
